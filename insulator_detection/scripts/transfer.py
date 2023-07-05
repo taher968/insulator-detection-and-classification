1e7b379acef6b22d8ed7ee3f1619321af53930d2
Taher Rampurawala <taherrampurawala5253@gmail.com>
2023-07-05 13:30:45 -0400

    script changes

--- insulator_detection/scripts/transfer.py
+++ insulator_detection/scripts/transfer.py
@@ -40,13 +40,26 @@ dest_train_xml_dir = 'XML/Training'
 dest_val_image_dir = 'Images/Validation'
 dest_val_xml_dir = 'XML/Validation'
 
-# Read training file list
-with open('insulator_train.txt', 'r') as file:
-    train_file_list = [os.path.splitext(line.strip())[0] for line in file.readlines()]
+# Read training file list from 'insulator_train.txt'
+with open('insulator_train.txt', 'r') as insulator_file:
+    insulator_train_file_list = [os.path.splitext(line.strip())[0] for line in insulator_file.readlines()]
+
+# Read training file list from 'non-insulator_train.txt'
+with open('non-insulator_train.txt', 'r') as non_insulator_file:
+    non_insulator_train_file_list = [os.path.splitext(line.strip())[0] for line in non_insulator_file.readlines()]
+
+# Combine both lists into train_file_list
+train_file_list = insulator_train_file_list + non_insulator_train_file_list
 
 # Read validation file list
 with open('insulator_val.txt', 'r') as file:
-    val_file_list = [os.path.splitext(line.strip())[0] for line in file.readlines()]
+    insulator_val_file_list = [os.path.splitext(line.strip())[0] for line in file.readlines()]
+
+with open('non-insulator_val.txt', 'r') as file:
+    non_insulator_val_file_list = [os.path.splitext(line.strip())[0] for line in file.readlines()]
+
+val_file_list = insulator_val_file_list + non_insulator_val_file_list
+
 
 # Transfer training files
 transfer_files(source_image_dir, source_xml_dir, dest_train_image_dir, dest_train_xml_dir, dest_val_image_dir, dest_val_xml_dir, train_file_list)
